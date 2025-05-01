d67b3824044d2096be6f5bc51ca4851672d091cb
Benjamin Bayarri <benjaminbayarri17@gmail.com>
2025-05-01 20:59:39 -0300

    refactor: Melee attack hitbox

--- Assets/Scripts/Player/AttackMelee/MeleeAttackHitbox.cs
+++ Assets/Scripts/Player/AttackMelee/MeleeAttackHitbox.cs
@@ -1,8 +1,25 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using Player;
 using UnityEngine;
 
 public class MeleeAttackHitbox : MonoBehaviour
 {
-    // Cuando entre en contaacto con un game object debe obtener su componente de vida y aplicar una cantidad de daño variable
+    private PlayerInputHandler inputHandler;
+    public int damage = 1;
+    // Cuando entre en contacto con un game object debe obtener su componente de vida y aplicar una cantidad de daño variable
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        EnemyHealth enemyHealth = GetComponent<EnemyHealth>();
+        if (enemyHealth != null && inputHandler.attackPressed)
+        {
+            enemyHealth.TakeDamage(damage);
+            Debug.Log("Damage was caused to the enemy");
+        }
+        else
+        {
+            Debug.Log("Enemy Health is null");
+        }
+    }
 }
