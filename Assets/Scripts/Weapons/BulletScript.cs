718913d1d78aafb05d590a71b1f6ac213b42d4c7
Emmazabala <emazabala08@gmail.com>
2025-05-27 20:11:24 -0300

    feat: Collision and Damage added to bullet

--- Assets/Scripts/Weapons/BulletScript.cs
+++ Assets/Scripts/Weapons/BulletScript.cs
@@ -4,16 +4,42 @@ using UnityEngine;
 
 public class BulletScript : MonoBehaviour
 {
-    [SerializeField] private float _lifeTime;
-    [SerializeField] private float _bulletSpeed;
+    [Header("Lifetime & Speed")]
+    [SerializeField] private float _lifeTime = 3f;
+    [SerializeField] private float _bulletSpeed = 10f;
 
-    void Start()
+    [Header("Damage")]
+    [SerializeField] private int _damage = 20;    
+    private void Start()
     {
-        Destroy(gameObject,_lifeTime);
+        Destroy(gameObject, _lifeTime);
     }
 
-    void Update()
+    private void Update()
     {
         transform.Translate(Vector2.right * _bulletSpeed * Time.deltaTime);
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+            return;
+
+        if (other.CompareTag("Enemy"))
+        {
+            
+            var health = other.GetComponent<Health>();
+            if (health != null)
+            {
+                health.TakeDamage(_damage);
+            }
+            
+            Destroy(gameObject);
+        }
+        else if (other.isTrigger == false)
+        {
+            // destruir la bala si golpea cualquier muro/obstáculo no trigger
+            Destroy(gameObject);
+        }
     }
 }
