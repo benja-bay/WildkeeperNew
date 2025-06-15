fd95bd9e20c2df84955b1979e1b3356dc03a1b91
Benjamin Bayarri <benjaminbayarri17@gmail.com>
2025-06-14 22:35:18 -0300

    feat: enemies animations implemented

--- Assets/Scripts/Enemies/EnemyAnimation.cs
+++ Assets/Scripts/Enemies/EnemyAnimation.cs
@@ -1,18 +1,56 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
+[RequireComponent(typeof(Animator))]
+[RequireComponent(typeof(NavMeshAgent))]
 public class EnemyAnimation : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    private Animator animator;
+    private NavMeshAgent agent;
+
+    private Vector2 lastDirection = Vector2.down;
+
+    private static readonly int IsMoving = Animator.StringToHash("IsMoving");
+    private static readonly int MoveX = Animator.StringToHash("MoveX");
+    private static readonly int MoveY = Animator.StringToHash("MoveY");
+    private static readonly int IsAttacking = Animator.StringToHash("IsAttacking");
+    private static readonly int AttackX = Animator.StringToHash("AttackX");
+    private static readonly int AttackY = Animator.StringToHash("AttackY");
+
+    void Awake()
     {
-        
+        animator = GetComponent<Animator>();
+        agent = GetComponent<NavMeshAgent>();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+        Vector2 velocity = new Vector2(agent.velocity.x, agent.velocity.y);
+        bool isMoving = velocity.sqrMagnitude > 0.01f;
+
+        animator.SetBool(IsMoving, isMoving);
+
+        if (isMoving)
+        {
+            lastDirection = velocity.normalized;
+        }
+
+        animator.SetFloat(MoveX, lastDirection.x);
+        animator.SetFloat(MoveY, lastDirection.y);
+    }
+    
+    public void PlayAttack()
+    {
+        animator.SetBool(IsMoving, false);
+        animator.SetBool(IsAttacking, true);
+        animator.SetFloat(AttackX, lastDirection.x);
+        animator.SetFloat(AttackY, lastDirection.y);
+        animator.CrossFade("Attack", 0.1f);
+    }
+
+    public void StopAttack()
+    {
+        animator.SetBool(IsAttacking, false);
     }
 }
+
